4504da78743f907a8156cce7e21fbc130e260025
pero.yan <pero.yan@shengyecapital.com>
2018-08-23 14:30:13 +0800

    1

---
+++ src/main/java/com/yzh/myeureka/MyeurekaApplication.java
@@ -0,0 +1,16 @@
+package com.yzh.myeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MyeurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyeurekaApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/yzh/myeureka/MyeurekaApplicationTests.java
@@ -0,0 +1,15 @@
+//package com.yzh.myeureka;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes = MyeurekaApplicationTests.class)
+//public class MyeurekaApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
